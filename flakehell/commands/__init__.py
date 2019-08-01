df2864ccda5b4b480ce51b36cd846bd41fed527c
Gram <master_fess@mail.ru>
2019-08-01 22:57:03 +0200

    +used and excluded commands listing

--- flakehell/commands/__init__.py
+++ flakehell/commands/__init__.py
@@ -1,20 +1,26 @@
 from types import MappingProxyType
 
+from ._excluded import excluded_command
 from ._installed import installed_command
 from ._lint import lint_command
 from ._show import show_command
+from ._used import used_command
 
 
 __all__ = [
     'COMMANDS',
+    'excluded_command',
     'installed_command',
     'lint_command',
     'show_command',
+    'used_command',
 ]
 
 
 COMMANDS = MappingProxyType(dict(
+    excluded=excluded_command,
     installed=installed_command,
     lint=lint_command,
     show=show_command,
+    used=used_command,
 ))
---
+++ flakehell/commands/_used.py
@@ -0,0 +1,20 @@
+from .._app import FlakeHellApplication
+from .._constants import NAME, VERSION, ExitCodes
+from .._discover import get_installed
+from .._plugin import get_plugin_rules
+from .._types import CommandResult
+
+
+def used_command(argv) -> CommandResult:
+    app = FlakeHellApplication(program=NAME, version=VERSION)
+    installed_plugins = sorted(get_installed(app=app), key=lambda p: p['name'])
+    if not installed_plugins:
+        return ExitCodes.NO_PLUGINS_INSTALLED, 'no plugins installed'
+    for plugin in installed_plugins:
+        rules = get_plugin_rules(
+            plugin_name=plugin['name'],
+            plugins=app.options.plugins,
+        )
+        if rules:
+            print(plugin['name'])
+    return 0, ''
