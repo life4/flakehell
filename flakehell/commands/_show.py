071fff328024ce812ee7166d1a78cd5fe3b927ce
Gram <master_fess@mail.ru>
2019-08-02 21:44:10 +0200

    make `show` pretty

--- flakehell/commands/_show.py
+++ flakehell/commands/_show.py
@@ -1,6 +1,15 @@
+import re
+
+from termcolor import colored
+
 from .._constants import ExitCodes
 from .._types import CommandResult
 from .._extractors import extract
+
+
+REX_CODE = re.compile(r'([A-Z]+)([0-9]+)')
+REX_PLACEHOLDER = re.compile(r'(\{[a-z0-9]+\}|\%[a-z])')
+REX_QUOTES = re.compile(r'([\"\'\`][\w\s\:\_\-\.]+[\"\'\`])')
 
 
 def show_command(argv) -> CommandResult:
@@ -14,5 +23,8 @@ def show_command(argv) -> CommandResult:
         return ExitCodes.NO_CODES, 'no codes found'
     width = max(len(code) for code in codes)
     for code, info in sorted(codes.items()):
+        code = REX_CODE.sub(colored(r'\1', 'blue') + colored(r'\2', 'cyan'), code)
+        info = REX_QUOTES.sub(colored(r'\1', 'yellow'), info)
+        info = REX_PLACEHOLDER.sub(colored(r'\1', 'green'), info)
         print('{} | {}'.format(code.ljust(width), info))
     return 0, ''
