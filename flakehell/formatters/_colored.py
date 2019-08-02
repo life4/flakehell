3f607cac049c5c5ef5176cdbe052b95719c72e6b
Gram <master_fess@mail.ru>
2019-08-02 19:46:16 +0200

    colorize source code

--- flakehell/formatters/_colored.py
+++ flakehell/formatters/_colored.py
@@ -2,6 +2,9 @@ import re
 
 from colorama import Style, Fore
 from flake8.formatting.default import Default
+from pygments import highlight
+from pygments.formatters import TerminalFormatter
+from pygments.lexers import PythonLexer
 
 from .._constants import COLORS
 
@@ -14,6 +17,8 @@ class ColoredFormatter(Default):
     def after_init(self):
         if self.options.format.lower() not in ('default', 'colored'):
             self.error_format = self.options.format
+        self._lexer = PythonLexer()
+        self._formatter = TerminalFormatter()
 
     def format(self, error):
         code = error.code
@@ -33,3 +38,25 @@ class ColoredFormatter(Default):
             row=Fore.CYAN + str(error.line_number) + Style.RESET_ALL,
             col=Fore.CYAN + str(error.column_number) + Style.RESET_ALL,
         )
+
+    def show_source(self, error) -> str:
+        """Called when ``--show-source`` option is provided."""
+        if not self._should_show_source(error):
+            return ''
+
+        formated_line = error.physical_line.lstrip()
+        adjust = len(error.physical_line) - len(formated_line)
+
+        code = highlight(
+            formated_line,
+            self._lexer,
+            self._formatter,
+        )
+
+        return '  {code}  {pointer}^'.format(
+            code=code,
+            pointer=' ' * (error.column_number - 1 - adjust),
+        )
+
+    def _should_show_source(self, error) -> bool:
+        return self.options.show_source and error.physical_line is not None
