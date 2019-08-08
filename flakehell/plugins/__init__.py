cf07d1533249566c8fe696b7c4793de98cce8fd9
Gram <master_fess@mail.ru>
2019-08-08 22:05:09 +0200

    +pylint checker

---
+++ flakehell/plugins/__init__.py
@@ -0,0 +1,16 @@
+
+from ._pylint import PyLintChecker
+# try:
+#     from ._pylint import PyLintChecker
+# except ImportError:
+#     PyLintChecker = None
+
+
+PLUGINS = dict(
+    pylint=PyLintChecker,
+)
+
+__all__ = [
+    'PLUGINS',
+    'PyLintChecker',
+]
