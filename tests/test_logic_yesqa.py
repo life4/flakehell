12356efc7953b68cdf4b7eae0d4c5f0fa00ee0b0
Gram <master_fess@mail.ru>
2020-03-10 13:20:00 +0100

    test a bit more

--- tests/test_logic_yesqa.py
+++ tests/test_logic_yesqa.py
@@ -13,8 +13,13 @@ def get_modified(content: str, path: Path) -> str:
 
 @pytest.mark.parametrize('content', [
     'print("hello")',
+    # don't add not ignored codes
+    'err=1',
+    # don't touch other codes
     'err=1 # noqa: E225',
+    # preserve case
     'err=1 # NoQA: E225',
+    # respect tokenization structure
     # 'err="# noqa: E117"',
 ])
 def test_not_modified(content: str, tmp_path: Path):
@@ -31,6 +36,10 @@ def test_not_modified(content: str, tmp_path: Path):
     ('err=1 # noqa: E117', 'err=1'),
     # case insensitive
     ('err=1 # NoQA: E117', 'err=1'),
+
+    # https://github.com/asottile/yesqa/blob/master/tests/yesqa_test.py
+    ('import os  # noqa: F401,X999\n', 'import os  # noqa: F401\n'),
+    ('import os  # noqa:F401,X999\n', 'import os  # noqa: F401\n'),
 ])
 def test_modified(given: str, expected: str, tmp_path: Path):
     assert get_modified(content=given, path=tmp_path) == expected
