f56e279934f194bf70a53f563a272a2e4b69c1de
Gram <master_fess@mail.ru>
2020-05-21 11:35:46 +0200

    Add Gitlab CI formatter

--- flakehell/formatters/__init__.py
+++ flakehell/formatters/__init__.py
@@ -1,5 +1,6 @@
 from ._baseline import BaseLineFormatter
 from ._colored import ColoredFormatter
+from ._gitlab import GitlabFormatter
 from ._grouped import GroupedFormatter
 from ._json import JSONFormatter
 from ._stat import StatFormatter
@@ -8,6 +9,7 @@ from ._stat import StatFormatter
 FORMATTERS = dict(
     baseline=BaseLineFormatter,
     colored=ColoredFormatter,
+    gitlab=GitlabFormatter,
     grouped=GroupedFormatter,
     json=JSONFormatter,
     stat=StatFormatter,
---
+++ flakehell/formatters/_gitlab.py
@@ -0,0 +1,46 @@
+import json
+from flake8.formatting.base import BaseFormatter
+from .._logic import make_baseline
+
+
+class GitlabFormatter(BaseFormatter):
+    error_format = '{code} {text}'
+
+    def start(self):
+        self._write('[')
+        self.newline = ''
+        self._first_line = True
+
+    def stop(self):
+        self._write('\n]\n')
+
+    def handle(self, error):
+        # redefined to never output source
+        line = self.format(error)
+        self._write(line)
+
+    def format(self, error):
+        filename = error.filename
+        if filename.startswith('./'):
+            filename = filename[2:]
+        digest = make_baseline(
+            path=filename,
+            code=error.code,
+            line=error.line_number,
+            context=error.physical_line,
+        )
+        text = self.error_format.format(code=error.code, text=error.text)
+        # docs.gitlab.com/ee/user/project/merge_requests/code_quality.html
+        result = json.dumps(dict(
+            description=text,
+            fingerprint=digest,
+            location={
+                'path': filename,
+                'lines': dict(begin=error.line_number),
+            },
+        ))
+        if self._first_line:
+            self._first_line = False
+        else:
+            result = ',\n' + result
+        return result
