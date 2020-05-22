41831b4dcf2ac5a4406573692dbeb14d82b2800c
Gram <master_fess@mail.ru>
2020-05-22 12:59:18 +0200

    bump version to 0.4.4

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ project = 'FlakeHell'
 copyright = '{}, Gram (@orsinium)'.format(date.today().year)
 author = 'Gram (@orsinium)'
 
-version = '0.4.3'
+version = '0.4.4'
 release = version
 
 language = None
