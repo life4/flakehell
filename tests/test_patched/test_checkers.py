7bb0b543c70b12bbd3ef7004222ffe72ea7e3d20
Gram <master_fess@mail.ru>
2020-05-22 09:41:18 +0200

    test a few things

---
+++ tests/test_patched/test_checkers.py
@@ -0,0 +1,45 @@
+from unittest import mock
+from flakehell._patched._checkers import FlakeHellFileChecker
+
+
+def test_nonexistent_file():
+    """Verify that checking non-existent file results in an error."""
+    c = FlakeHellFileChecker(
+        filename='foobar.py',
+        check_type='ast',
+        check={
+            'plugin_name': 'flake8-example',
+            'name': 'something',
+            'plugin': FlakeHellFileChecker,
+        },
+        options=None,
+    )
+
+    assert c.processor is None
+    assert not c.should_process
+    assert len(c.results) == 1
+    error = c.results[0]
+    assert error[0] == 'E902'
+
+
+def test_catches_exception_on_invalid_syntax(tmp_path):
+    code_path = tmp_path / 'example.py'
+    code_path.write_text('I exist!')
+    plugin = {
+        'name': 'failure',
+        'plugin_name': 'failure',
+        'parameters': dict(),
+        'plugin': mock.MagicMock(side_effect=ValueError),
+    }
+    options = mock.MagicMock()
+    options.safe = False
+    fchecker = FlakeHellFileChecker(
+        filename=str(code_path),
+        check_type='ast',
+        check=plugin,
+        options=options,
+    )
+    assert fchecker.should_process is True
+    assert fchecker.processor is not None
+    fchecker.run_checks()
+    assert fchecker.results == [('E999', 1, 6, 'SyntaxError: invalid syntax', 'I exist!')]
