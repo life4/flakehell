07e6ea88c3f24bd4a25b215ca63617e290140065
gram <mail@orsinium.dev>
2021-01-11 12:56:26 +0100

    bump version to 0.8.0

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ project = 'FlakeHell'
 copyright = '{}, Gram (@orsinium)'.format(date.today().year)
 author = 'Gram (@orsinium)'
 
-version = '0.7.1'
+version = '0.8.0'
 release = version
 
 language = None
